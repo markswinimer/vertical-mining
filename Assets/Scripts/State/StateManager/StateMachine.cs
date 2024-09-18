a0c0662a39dc31e8a004776daacfbdc3cb171954
Ryaneklipse <rchild0916@gmail.com>
2024-09-18 06:21:43 -0400

    Update enemy script, enemy spawner

--- Assets/Scripts/State/StateManager/StateMachine.cs
+++ Assets/Scripts/State/StateManager/StateMachine.cs
@@ -3,34 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StateMachine {
-    public State state;
+	public State state;
 
-    public void Set(State newState, bool forceReset = false)
-    {
-        if (state != newState || forceReset)
-        {
-            state?.Exit();
-            state = newState;
-            state.Initialize(this);
-            state.Enter();
-        }
-    }
+	public void Set(State newState, bool forceReset = false)
+	{
+		if(newState == null) return;
+		if (state != newState || forceReset)
+		{
+			state?.Exit();
+			state = newState;
+			state.Initialize(this);
+			state.Enter();
+		}
+	}
 
-    public List<State> GetActiveStateBranch(List<State> list = null)
-    {
-        if (list == null)
-        {
-            list = new List<State>();
-        }
+	public List<State> GetActiveStateBranch(List<State> list = null)
+	{
+		if (list == null)
+		{
+			list = new List<State>();
+		}
 
-        if (state == null)
-        {
-            return list;
-        }
-        else 
-        {
-            list.Add(state);
-            return state.machine.GetActiveStateBranch(list);
-        }
-    }
+		if (state == null)
+		{
+			return list;
+		}
+		else 
+		{
+			list.Add(state);
+			return state.machine.GetActiveStateBranch(list);
+		}
+	}
 }
